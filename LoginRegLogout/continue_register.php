caea48b05dc361b8d48207e61b1d8d0aa4628dd0
Tawhidur Noor <56801563+tawhidurnoor@users.noreply.github.com>
2020-08-26 13:14:24 +0600

    Third Minorf commit

--- LoginRegLogout/continue_register.php
+++ LoginRegLogout/continue_register.php
@@ -9,19 +9,34 @@ $email = $_POST['email'];
 $pass_1 = $_POST['pass_1'];
 $pass_2 = $_POST['pass_2'];
 
-if(!empty($_POST['u_type'])) { 
-    $u_type = $_POST['u_type'];
-}else{
-    $u_type = "individual";
+
+$sql = "SELECT * FROM users WHERE email='$email'";
+$result = mysqli_query($conn, $sql);
+
+if (mysqli_num_rows($result) > 0) {
+    header("Location:registration.php?error=1");
+} else {
+    if (strcmp($pass_1, $pass_2) != 0) {
+
+        header("Location:registration.php?error=2");
+    } else {
+
+        if (!empty($_POST['u_type'])) {
+            $u_type = $_POST['u_type'];
+        } else {
+            $u_type = "individual";
+        }
+
+        $pro_pic = "avatar.png";
+
+        $sql2 = "INSERT INTO users(f_name, l_name, email, pass_1, pass_2, u_type, pro_pic) VALUES ('$f_name', '$l_name', '$email', '$pass_1', '$pass_2', '$u_type', '$pro_pic')";
+
+
+        mysqli_query($conn, $sql2);
+
+        header("Location:login.php");
+    }
 }
 
-$pro_pic = "avatar.png";
-
-$sql = "INSERT INTO users(f_name, l_name, email, pass_1, pass_2, u_type, pro_pic) VALUES ('$f_name', '$l_name', '$email', '$pass_1', '$pass_2', '$u_type', '$pro_pic')";
-
-
-mysqli_query($conn,$sql);
-
-header("Location:login.php");
 
 ?>
--- LoginRegLogout/registration.php
+++ LoginRegLogout/registration.php
@@ -1,99 +1,113 @@
 <!DOCTYPE html>
 <html lang="en">
-  <head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <title>Registration | Escapers</title>
-    <!-- plugins:css -->
-    
-    <!-- endinject -->
-    <!-- plugin css for this page -->
-    <!-- End plugin css for this page -->
-    <!-- inject:css -->
-    <link rel="stylesheet" href="assets/css/style.css">
-    <!-- endinject -->
-  </head>
-  <body>
-    <div class="container-scroller">
-      <div class="container-fluid page-body-wrapper full-page-wrapper">
-        <div class="content-wrapper d-flex align-items-center auth register-bg-1 theme-one">
-          <div class="row w-100">
-            <div class="col-lg-4 mx-auto">
-              <div class="auto-form-wrapper">
-                <form action="continue_register.php" method="post">
-                  <h3>REGISTRATION</h3>
-                  <div class="form-group">
-                    <div class="input-group">
-                      <input type="text" name="f_name" class="form-control" placeholder="First Name">
-                      <div class="input-group-append">
-                        <span class="input-group-text">
-                          <i class="mdi mdi-check-circle-outline"></i>
-                        </span>
-                      </div>
+
+<head>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <title>Registration | Escapers</title>
+  <!-- plugins:css -->
+
+  <!-- endinject -->
+  <!-- plugin css for this page -->
+  <!-- End plugin css for this page -->
+  <!-- inject:css -->
+  <link rel="stylesheet" href="assets/css/style.css">
+  <!-- endinject -->
+</head>
+
+<body>
+  <div class="container-scroller">
+    <div class="container-fluid page-body-wrapper full-page-wrapper">
+      <div class="content-wrapper d-flex align-items-center auth register-bg-1 theme-one">
+        <div class="row w-100">
+          <div class="col-lg-4 mx-auto">
+            <div class="auto-form-wrapper">
+              <form action="continue_register.php" method="post">
+                <h3>REGISTRATION</h3>
+                <div class="form-group">
+                  <div class="input-group">
+                    <input type="text" name="f_name" class="form-control" placeholder="First Name">
+                    <div class="input-group-append">
+                      <span class="input-group-text">
+                        <i class="mdi mdi-check-circle-outline"></i>
+                      </span>
                     </div>
                   </div>
-                  <div class="form-group">
-                    <div class="input-group">
-                      <input type="text" name="l_name" class="form-control" placeholder="Last Name">
-                      <div class="input-group-append">
-                        <span class="input-group-text">
-                          <i class="mdi mdi-check-circle-outline"></i>
-                        </span>
-                      </div>
+                </div>
+                <div class="form-group">
+                  <div class="input-group">
+                    <input type="text" name="l_name" class="form-control" placeholder="Last Name">
+                    <div class="input-group-append">
+                      <span class="input-group-text">
+                        <i class="mdi mdi-check-circle-outline"></i>
+                      </span>
                     </div>
                   </div>
-                  <div class="form-group">
-                    <div class="input-group">
-                      <input type="email" name="email" class="form-control" placeholder="Email">
-                      <div class="input-group-append">
-                        <span class="input-group-text">
-                          <i class="mdi mdi-check-circle-outline"></i>
-                        </span>
-                      </div>
+                </div>
+                <div class="form-group">
+                  <div class="input-group">
+                    <input type="email" name="email" class="form-control" placeholder="Email">
+                    <div class="input-group-append">
+                      <span class="input-group-text">
+                        <i class="mdi mdi-check-circle-outline"></i>
+                      </span>
                     </div>
                   </div>
-                  <div class="form-group">
-                    <div class="input-group">
-                      <input type="password" name="pass_1" class="form-control" placeholder="Password">
-                      <div class="input-group-append">
-                        <span class="input-group-text">
-                          <i class="mdi mdi-check-circle-outline"></i>
-                        </span>
-                      </div>
+                </div>
+                <div class="form-group">
+                  <div class="input-group">
+                    <input type="password" name="pass_1" class="form-control" placeholder="Password">
+                    <div class="input-group-append">
+                      <span class="input-group-text">
+                        <i class="mdi mdi-check-circle-outline"></i>
+                      </span>
                     </div>
                   </div>
-                  <div class="form-group">
-                    <div class="input-group">
-                      <input type="password" name="pass_2" class="form-control" placeholder="Confirm Password">
-                      <div class="input-group-append">
-                        <span class="input-group-text">
-                          <i class="mdi mdi-check-circle-outline"></i>
-                        </span>
-                      </div>
+                </div>
+                <div class="form-group">
+                  <div class="input-group">
+                    <input type="password" name="pass_2" class="form-control" placeholder="Confirm Password">
+                    <div class="input-group-append">
+                      <span class="input-group-text">
+                        <i class="mdi mdi-check-circle-outline"></i>
+                      </span>
                     </div>
                   </div>
-                  <div class="form-group">
-                     <input type="checkbox" id="u_type" name="u_type" value="organization">
-                     <span class="text-small">I am registring on behalf of a organization.</span>
-                  </div>
-                  <div class="form-group">
-                    <button type="submit" name="registration" class="btn btn-primary submit-btn btn-block">Continue</button>
-                  </div>
-                  <div class="text-block text-center my-3">
-                    <span class="text-small font-weight-semibold">Already a member ?</span>
-                    <a href="login.php" class="text-black text-small">Login to your account</a>
-                  </div>
-                </form>
-              </div>
-              <br><p class="footer-text text-center">copyright © <b>Escapers</b>. All rights reserved.</p>
+                </div>
+                <div class="form-group">
+                  <?php if (null != isset($_GET['error'])) {
+                    if($_GET['error'] == "1"){
+                      echo '<p class="text-danger">Email already taken</p>';
+                    }else{
+                      echo '<p class="text-danger">Passwords Didn not match</p>';
+                    }
+                
+                  } ?>
+                </div>
+                <div class="form-group">
+                  <input type="checkbox" id="u_type" name="u_type" value="organization">
+                  <span class="text-small">I am registring on behalf of a organization.</span>
+                </div>
+                <div class="form-group">
+                  <button type="submit" name="registration" class="btn btn-primary submit-btn btn-block">Continue</button>
+                </div>
+                <div class="text-block text-center my-3">
+                  <span class="text-small font-weight-semibold">Already a member ?</span>
+                  <a href="login.php" class="text-black text-small">Login to your account</a>
+                </div>
+              </form>
             </div>
+            <br>
+            <p class="footer-text text-center">copyright © <b>Escapers</b>. All rights reserved.</p>
           </div>
         </div>
-        <!-- content-wrapper ends -->
       </div>
-      <!-- page-body-wrapper ends -->
+      <!-- content-wrapper ends -->
     </div>
-    <!-- container-scroller -->
-  </body>
+    <!-- page-body-wrapper ends -->
+  </div>
+  <!-- container-scroller -->
+</body>
+
 </html>
